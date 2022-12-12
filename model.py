12e3171b5744b11cc33814edf292091b01f167b6
Jakob Gal <jg.frankovic@gmail.com>
2022-12-12 13:53:01 +0100

    Dodal nekaj datotek

--- model.py
+++ model.py
@@ -1,10 +1,14 @@
 import sqlite3
- 
-
+import baza
+#V modelu bo delal vse kar bo povezano z bazo.
 conn = sqlite3.connect("baza.sqlite3")
 # TODO: Tukaj moramo ustvarit bazo, če je še ni.
+baza.pripravi_vse(conn)
 
 class Model:
     def dobi_vse_uporabnike(self):
-        
-        pass
+        with conn:
+            cur = conn.execute(""" 
+            SELECT * FROM uporabnik
+            """)
+            return cur.fetchall()
--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,4 +1,13 @@
 import bottle
-print(bottle)
-
+import model
 #NIKOLI NE IMPORTAJ sqlite3 v spletni vmesnik
+
+glavni_model = model.Model()
+
+@bottle.get("/")
+def glavna_stran():
+    return bottle.template("glavna.html", uporabniki = podatki)
+
+bottle.run(debug = True, reloader = True)
+
+
