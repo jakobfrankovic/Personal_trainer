939952247b7e3e508534eec78f7534a6ed60b0cc
Jakob Gal <jg.frankovic@gmail.com>
2022-12-12 13:56:15 +0100

    To kar smo naredili na vajah 12.12

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import sqlite3
 import baza
 #V modelu bo delal vse kar bo povezano z bazo.
-conn = sqlite3.connect("baza.sqlite3")
+conn = sqlite3.connect("baza.sqlite3") #connecti se na projekt
 # TODO: Tukaj moramo ustvarit bazo, če je še ni.
 baza.pripravi_vse(conn)
 
@@ -9,6 +9,6 @@ class Model:
     def dobi_vse_uporabnike(self):
         with conn:
             cur = conn.execute(""" 
-            SELECT * FROM uporabnik
+            SELECT * FROM uporabnisko_ime
             """)
             return cur.fetchall()
--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -6,6 +6,7 @@ glavni_model = model.Model()
 
 @bottle.get("/")
 def glavna_stran():
+    podatki = glavni_model.dobi_vse_uporabnike()
     return bottle.template("glavna.html", uporabniki = podatki)
 
 bottle.run(debug = True, reloader = True)
